2ee65828b239adbd2a596752358b9b15a14cb4ef
Daniel Langio <langio.tehiniavo@gmail.com>
2026-04-27 20:38:03 +0300

    chore: spring initialization

---
+++ src/main/java/code/mafoaka/fidio/FidioApiApplication.java
@@ -0,0 +1,12 @@
+package code.mafoaka.fidio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidioApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FidioApiApplication.class, args);
+  }
+}
---
+++ src/test/java/code/mafoaka/fidio/FidioApiApplicationTests.java
@@ -0,0 +1,11 @@
+package code.mafoaka.fidio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidioApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
